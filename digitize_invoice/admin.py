3fff2a46a32e43fadb01e05d87d3866d84147fe4
sunidhibn <sunidhi.bn@gmail.com>
2020-09-28 14:32:27 +0530

    working upload API

--- digitize_invoice/admin.py
+++ digitize_invoice/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Invoice
 # Register your models here.
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display=('id','invoice','created_at')
+
+admin.site.register(Invoice,InvoiceAdmin)
--- digitize_invoice/models.py
+++ digitize_invoice/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Invoice(models.Model):
+    
+    invoice = models.FileField(blank=False, null=False, upload_to='documents/')
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.invoice.name
+
+class Digitizer(models.Model):
+    invoice= models.ForeignKey(Invoice,on_delete=models.CASCADE,related_name="digized")
+    data=models.JSONField(default=dict)
+    status=models.BooleanField(default=False)
+    digitized_at=models.DateTimeField()
